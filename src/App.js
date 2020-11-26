5008cf47909db3a5e581568bb80eb3a8fe7bf5e6
MohammedYacub1029 <39052590+MohammedYacub1029@users.noreply.github.com>
2020-11-26 23:38:24 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-    import React, { Component } from "react";
-    import Question from "./Question/Question";
-    import Answer from "./Answer/Answer";
-    import Next from "./Next/Next";
-    import Previous from "./previous/Previous";
-    import Result from "./Result/Result";
-    import "./App.css";
-    const Questions=(props)=> {
-      const { questions = [], current_question = 0 } = props;
+import React, { Component } from "react";
+import Question from "./Question/Question";
+import Answer from "./Answer/Answer";
+import Next from "./Next/Next";
+import Previous from "./previous/Previous";
+import Result from "./Result/Result";
+import "./App.css";
+const Questions = (props) => {
+  const { questions = [], current_question = 0 } = props;
   let questionss = questions.map((ques, id) => {
     return (
       <div key={id} data-hidden={id !== current_question}>
-      <p className="question-no">  {current_question + 1} / {questions.length}</p>
-        <Question  className="questions" question={ques.question} />
+        <p className="question-no">
+          {" "}
+          {current_question + 1} / {questions.length}
+        </p>
+        <Question className="questions" question={ques.question} />
         <ol>{<Answer setAnswer={props.setAnswer} ansloop={ques.answers} />}</ol>
       </div>
     );
   });
   return <ol className="ol">{questionss}</ol>;
-}
+};
 class App extends Component {
   constructor(props) {
     super(props);
@@ -34,25 +37,23 @@ class App extends Component {
       },
       current_question: 0,
       selected_answer: [],
-      total_score : 0
+      total_score: 0
     };
   }
   componentDidMount() {
-   
     fetch("https://api.myjson.com/bins/ddy3p")
-      .then(response => {
+      .then((response) => {
         return response.json();
       })
-      .then(jstr => {
+      .then((jstr) => {
         this.setState({ quiz: jstr });
-       
       });
   }
-      addtotalscore=(totalscore)=>{
-        this.setState({
-          total_score: totalscore++
-        })
-      }
+  addtotalscore = (totalscore) => {
+    this.setState({
+      total_score: totalscore++
+    });
+  };
 
   nextQuestion() {
     let { current_question, quiz } = this.state;
@@ -72,8 +73,8 @@ class App extends Component {
     });
   }
 
-  setSelectedAnswer = answer => {
-    this.setState(prevState => ({
+  setSelectedAnswer = (answer) => {
+    this.setState((prevState) => ({
       selected_answer: [...prevState.selected_answer, answer]
     }));
   };
@@ -81,26 +82,26 @@ class App extends Component {
   render() {
     return (
       <div>
-      <div className="header">
+        <div className="header">
           <p className="quiz-title">{this.state.quiz.title}</p>
-          </div>
-      <div className="app">
-        <Previous onClick={() => this.previousQuestion()} />
-        <div>
-          
-          <Questions
-            questions={this.state.quiz.questions}
-            current_question={this.state.current_question}
-            setAnswer={this.setSelectedAnswer}
-          />
         </div>
-        <Next onClick={() => this.nextQuestion()} />
-         <Result totalscores={this.addtotalscore}  {...this.state} />
-        <h3 className="final-output">Total Marks you have scored: {this.state.total_score}</h3>
-        
-      </div>
+        <div className="app">
+          <Previous onClick={() => this.previousQuestion()} />
+          <div>
+            <Questions
+              questions={this.state.quiz.questions}
+              current_question={this.state.current_question}
+              setAnswer={this.setSelectedAnswer}
+            />
+          </div>
+          <Next onClick={() => this.nextQuestion()} />
+          <Result totalscores={this.addtotalscore} {...this.state} />
+          <h3 className="final-output">
+            Total Marks you have scored: {this.state.total_score}
+          </h3>
+        </div>
       </div>
     );
   }
 }
-    export default App;
+export default App;
